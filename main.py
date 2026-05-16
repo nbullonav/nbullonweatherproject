cf87024218e304207ae7c9d0db15cbba8d9dc98b
nbullonav <nbullon@live.com>
2026-05-16 17:24:49 -0500

    Update main.py
    agregar sst e icen

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ finaldata.drop(columns=['id','elevation','identifiers','timezone'], inplace=True
 finaldata.set_index('time', inplace=True)
 
 
-#### EDA
-## PRCP
+#### EDA ####
+
+### PRCP
 max_prcp = finaldata['prcp'].quantile(0.999)
 max_prcp
 min_prcp = finaldata['prcp'].quantile(0.001)
@@ -31,7 +32,8 @@ min_prcp
 df2 = finaldata[finaldata['prcp'] > min_prcp]
 prcp_avg = df2['prcp'].mean().round(2)
 finaldata.loc[finaldata['prcp'].isna(), 'prcp'] = prcp_avg
-## TEMP
+
+### TEMP
 max_Temp = finaldata['temp'].quantile(0.999)
 max_Temp
 min_Temp = finaldata['temp'].quantile(0.001)
@@ -39,7 +41,8 @@ min_Temp
 df2 = finaldata[finaldata['temp'] > min_Temp]
 Temp_avg = df2['temp'].mean().round(2)
 finaldata.loc[finaldata['temp'].isna(), 'temp'] = Temp_avg
-## PRES
+
+### PRES
 max_pres = finaldata['pres'].quantile(0.999)
 max_pres
 min_pres = finaldata['pres'].quantile(0.001)
@@ -48,8 +51,18 @@ df2 = finaldata[finaldata['pres'] > min_pres]
 pres_avg = df2['pres'].mean().round(2)
 finaldata.loc[finaldata['pres'].isna(), 'pres'] = pres_avg
 
+## 1.2. Captura de datos - Dataset 2 (ONI data de NOAA.gov)
+df_sst = pd.read_csv("https://www.cpc.ncep.noaa.gov/data/indices/ersst5.nino.mth.91-20.ascii", sep='\s+',
+                     names=["YR", "MON", "NINO12","NINO12ANOM", "NINO3","NINO3ANOM", "NINO4","NINO4ANOM", "NINO34","NINO34ANOM"], header=0)
+df_sst_final = df_sst[['YR','MON','NINO12',"NINO12ANOM","NINO34","NINO34ANOM"]]
+df_sst_final['time'] = df_sst_final['YR'].astype(str) + '-' + df_sst_final['MON'].astype(str)
+df_sst_final['time'] = pd.to_datetime(df_sst_final['time'], format='%Y-%m')
+df_sst_final.set_index('time', inplace=True)
 
-
+df_icen = pd.read_csv("http://met.igp.gob.pe/datos/ICEN.txt", sep=r"\s+", skiprows=5 ,header=None,names=["YR","MON","ICEN"])
+df_icen['time'] = df_icen['YR'].astype(str) + '-' + df_icen['MON'].astype(str)
+df_icen['time'] = pd.to_datetime(df_icen['time'], format='%Y-%m')
+df_icen.set_index('time', inplace=True)
 
 
 ## salvar en .csv
